cd2fc6ad375248cb822738bd93bd3137ceac6a1a
Cfrolich <cfro1212@gmail.com>
2023-06-05 18:31:46 -0500

    Added print for new comments

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ import praw
 import os
 
 reddit = praw.Reddit(
-  clientId = os.getenv('clientID')
+  clientId     = os.getenv('clientID'),
+  clientSecret = os.getenv('clientSecret'),
+  username     = os.getenv('username'),
+  password     = os.getenv('password'),
+  clientId     = os.getenv('clientID'),
+  userAgent    = "<EmojiShamerBot1.0>"
 )
+
+subreddit = reddit.subreddit("EmojiShamerBot")
+
+for comment in subreddit.stream.comments(skip_existing=True):
+    print(comment.body)
